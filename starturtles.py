aeeb99f8e0ceec9b9be5126eb2df33fb0f97e729
alejandrorebolledo <54950946+alejandrorebolledo@users.noreply.github.com>
2021-05-05 10:30:05 -0700

    Create starturtles.py

---
+++ starturtles.py
@@ -0,0 +1,20 @@
+import turtle
+ 
+board = turtle.Turtle()
+ 
+board.forward(200) # draw base
+ 
+board.left(-120)
+board.forward(200)
+ 
+board.left(-120)
+board.forward(200)
+
+t = turtle.Turtle()
+t.penup()
+t.goto(100.0)
+t.pendown()
+t.color(blue)
+
+ 
+turtle.done()
